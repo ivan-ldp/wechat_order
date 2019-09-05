8abef3fbc22834c90ff9ff3e8fbdc0134d91606d
ivan <Ivan_ldp@163.com>
2019-09-05 12:06:01 +0800

    2

---
+++ src/main/java/com/kgc/wechatorder/WechatorderApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.wechatorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatorderApplication.class, args);
+    }
+
+}
