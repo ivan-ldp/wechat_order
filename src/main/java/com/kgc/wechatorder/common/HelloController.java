87d865d23b9800687d1554e1ada6251445db9113
yu <1962840807@qq.com>
2019-09-06 09:09:27 +0800

    2019/9/6

--- src/main/java/com/kgc/wechatorder/common/HelloController.java
+++ src/main/java/com/kgc/wechatorder/common/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
+    //您好
     public  String hello(){
         return "hello";
     }
