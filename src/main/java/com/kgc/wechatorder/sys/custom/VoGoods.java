85cfca16985968a980b4d74ec663e1544850282a
yu <1962840807@qq.com>
2019-09-18 09:08:26 +0800

    2019/9/18 goodsController 10:43

---
+++ src/main/java/com/kgc/wechatorder/sys/custom/VoGoods.java
@@ -0,0 +1,31 @@
+package com.kgc.wechatorder.sys.custom;
+
+public class VoGoods {
+    private int status;
+    private String goodsName;
+    private int orderId;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
