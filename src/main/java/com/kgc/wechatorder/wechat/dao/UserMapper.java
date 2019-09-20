64924b1dd0e2a0942449c8e36ff25c26e492b809
yu <1962840807@qq.com>
2019-09-20 09:27:32 +0800

    2019/9/20 goodsController 10:43

--- src/main/java/com/kgc/wechatorder/wechat/dao/UserMapper.java
+++ src/main/java/com/kgc/wechatorder/wechat/dao/UserMapper.java
@@ -5,14 +5,37 @@ import com.kgc.wechatorder.wechat.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
+ * 用户表 DAO层
  * Created By Ivan_ldp@163.com on 2019/9/10 9:06
  */
 @Mapper
 public interface UserMapper {
-
+    /**
+     * 按openID保存用户信息
+     * @param openID
+     * @return
+     */
     User queryByOpenId(String openID);
+
+    /**
+     * 按userId保存用户信息
+     * @param userId
+     * @return
+     */
     User queryByUserId(String userId);
+
+    /**
+     * 添加用户
+     * @param user
+     * @return
+     */
     int save(User user);
+
+    /**
+     * 更新用户信息
+     * @param user
+     * @return
+     */
     int update(User user);
 
 }
--- src/main/java/com/kgc/wechatorder/wechat/dao/UserTokenMapper.java
+++ src/main/java/com/kgc/wechatorder/wechat/dao/UserTokenMapper.java
@@ -4,13 +4,37 @@ import com.kgc.wechatorder.wechat.pojo.UserToken;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
+ * token DAO层
  * Created By Ivan_ldp@163.com on 2019/9/10 16:15
  */
 @Mapper
 public interface UserTokenMapper {
+    /**
+     * 按userId查询用户token信息
+     * @param userId
+     * @return
+     */
     UserToken queryByUserId(Integer userId);
+
+    /**
+     * 按token查询用户token信息
+     * @param token
+     * @return
+     */
     UserToken queryByToken(String token);
+
+    /**
+     * 增加token信息
+     * @param userToken
+     * @return
+     */
     int save(UserToken userToken);
+
+    /**
+     * 更新token信息
+     * @param userToken
+     * @return
+     */
     int update(UserToken userToken);
 
 
